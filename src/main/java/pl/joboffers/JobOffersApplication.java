e4c12427359d8e06bbc6acd0b1e2bade4e46fd7a
adamropiak <adam.ropiak@onet.pl>
2026-07-01 10:06:33 +0200

    First commit with packages and empty facade classes

---
+++ src/main/java/pl/joboffers/JobOffersApplication.java
@@ -0,0 +1,9 @@
+package pl.joboffers;
+
+
+public class JobOffersApplication {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/pl/joboffers/domain/joboffers/JobOfferFacade.java
@@ -0,0 +1,4 @@
+package pl.joboffers.domain.joboffers;
+
+public class JobOfferFacade {
+}
