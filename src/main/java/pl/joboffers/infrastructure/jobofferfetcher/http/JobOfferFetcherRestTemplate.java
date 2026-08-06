52100af329351dc46f6e3a4f71d0b20bac925cda
adamropiak <adam.ropiak@onet.pl>
2026-08-06 21:17:03 +0200

    refactored JobOfferFetcherRestTemplate

--- src/main/java/pl/joboffers/infrastructure/jobofferfetcher/http/JobOfferFetcherRestTemplate.java
+++ src/main/java/pl/joboffers/infrastructure/jobofferfetcher/http/JobOfferFetcherRestTemplate.java
@@ -17,37 +17,49 @@ import java.util.List;
 @Log4j2
 public class JobOfferFetcherRestTemplate implements JobOfferFetcher {
 
+    private final String URL_ENDPOINT_PATH = "/offers";
+
     private final RestTemplate restTemplate;
     private final String uri;
     private final int port;
 
+
+
     @Override
     public List<JobOfferResponseDto> fetchAllJobOffers() {
         log.info("Fetching starts.");
-        String urlForService = getUrlForService("/offers");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+        final HttpEntity<HttpHeaders> requestEntity = new HttpEntity<>(headers);
         try {
-            final HttpEntity<HttpHeaders> requestEntity = new HttpEntity<>(headers);
-            final String url = UriComponentsBuilder.fromHttpUrl(urlForService)
-                    .toUriString();
-            ResponseEntity<List<JobOfferResponseDto>> response = restTemplate.exchange(
-                    url,
-                    HttpMethod.GET,
-                    requestEntity,
-                    new ParameterizedTypeReference<List<JobOfferResponseDto>>() {
-                    });
-            final List<JobOfferResponseDto> results = response.getBody();
-            if (results == null) {
-                log.info("Returned list is empty.");
-                return Collections.emptyList();
-            }
-            log.info("Fetching completed.");
-            return results;
+            ResponseEntity<List<JobOfferResponseDto>> response = getListResponseEntity(requestEntity);
+            return getJobOfferResponseDto(response);
         } catch (ResourceAccessException error) {
             log.error("Error during fetching: " + error.getMessage());
             return Collections.emptyList();
         }
+    }
+
+    private static List<JobOfferResponseDto> getJobOfferResponseDto(ResponseEntity<List<JobOfferResponseDto>> response) {
+        final List<JobOfferResponseDto> results = response.getBody();
+        if (results == null) {
+            log.info("Returned list is empty.");
+            return Collections.emptyList();
+        }
+        log.info("Fetching completed.");
+        return results;
+    }
+
+    private ResponseEntity<List<JobOfferResponseDto>> getListResponseEntity(HttpEntity<HttpHeaders> requestEntity) {
+        String urlForService = getUrlForService(URL_ENDPOINT_PATH);
+        final String url = UriComponentsBuilder.fromHttpUrl(urlForService)
+                .toUriString();
+        return restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                requestEntity,
+                new ParameterizedTypeReference<List<JobOfferResponseDto>>() {
+                });
     }
 
     private String getUrlForService(String service) {
